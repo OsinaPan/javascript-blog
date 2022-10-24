a4c1f74394b56ee669842e97755dfcf5d8f518f7
Michal Osinski <michal.osinski94@gmail.com>
2022-10-24 19:43:53 +0200

    add remove article

--- js/script.js
+++ js/script.js
@@ -8,8 +8,18 @@ const titleClickHandler = function(event){
     console.log(event);
 
   /* remove class 'active' from all article links */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clocked link */
   /* remove class 'article' from all articles */
+  const activeArticles = document.querySelectorAll('.post');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
   /* find the correct article using the selector (value of 'href' attribute) */
   /* add class 'article' to the correct article */
