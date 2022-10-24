9a8a8ba2cf19f43f14401347ee0f7535cc7bca4b
Michal Osinski <michal.osinski94@gmail.com>
2022-10-24 20:00:38 +0200

    add clickedElement

--- js/script.js
+++ js/script.js
@@ -7,14 +7,19 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
 
-  /* remove class 'active' from all article links */
+  /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clocked link */
-  /* remove class 'article' from all articles */
+  /* [DONE] add class 'active' to the clicked link */
+  const clickedElement = this;
+
+  clickedElement.classList.add('active');
+  console.log(clickedElement);
+
+  /* [DONE]remove class 'article' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
   for(let activeArticle of activeArticles){
