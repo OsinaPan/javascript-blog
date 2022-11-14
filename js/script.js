41bc464efacb77305cf02cf47af8456ef16a76e6
Michal Osinski <michal.osinski94@gmail.com>
2022-11-14 19:06:24 +0100

    fixed link problem

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-// document.getElementById('test-button').addEventListener('click', function(){
-//     const links = document.querySelectorAll('.titles a');
-//     console.log('links:', links);
-//   });
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+const titleClickHandler = function (event) {
+
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
+  for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
+
   /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   console.log(clickedElement);
   /* [DONE]remove class 'article' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
-  for(let activeArticle of activeArticles){
+  for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
   /* [DONE] get 'href' attribute from the clicked link */
@@ -31,63 +32,42 @@ const titleClickHandler = function(event){
   console.log(targetArticle);
   /* [DONE] add class 'article' to the correct article */
   targetArticle.classList.add('active');
-  }
-  
-  //const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+}
 
-  const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+function generateTitleLinks() {
 
-  function generateTitleLinks(){
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
 
-    /* [DONE] remove contents of titleList */
-    const titleList = document.querySelector(optTitleListSelector);
+  /* [DONE] for each article */
+  let html = '';
 
-    function clearTitleList(){
-      document.querySelector(optTitleListSelector).innerHTML = '';
-    }
+  const articles = document.querySelectorAll(optArticleSelector);
+  for (let article of articles) {
 
-    clearTitleList();
-
-    console.log(optTitleListSelector);
-    /* [DONE] for each article */
-    const articles = document.querySelector(optArticleSelector);
-
-    for(let article of articles){
-      article.classList.remove('active');
-    }
-    console.log(articles);
-
-    let html = '';
-
-    for(let article of articles){
     /* [DONE] get the article id */
-    const articleId = clickedElement.getAttribute('id');
+    const articleId = article.getAttribute('id');
 
-    console.log(articleId);
-    /* [DONE] find the title element */
-    const targetArticle = document.querySelector(articleId);
-
-    console.log(targetArticle);
     /* [DONE] get the title from the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
     /* [DONE] create HTML of the link */
-    //const linkHTML = '<li><a href="#"><span></span></a></li>';                    Informacja dla mnie jak to się rozbudowuje
-    //const linkHTML = '<li><a href="#' + '"><span>' + '</span></a></li>';                      -||-
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
     console.log(linkHTML)
     /* [DONE] insert link into titleList */
     html = html + linkHTML;
   }
-  titleList.innerHTML = html;
-}
 
-  generateTitleLinks();
+  titleList.innerHTML = html;
 
   const links = document.querySelectorAll('.titles a');
+
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
+
+}
+
+generateTitleLinks();
