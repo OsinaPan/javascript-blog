8b689ddaab91506c46349e8595b411ecf060f46e
Michal Osinski <michal.osinski94@gmail.com>
2022-11-22 19:44:18 +0100

    create function calculateTagsParams

--- js/script.js
+++ js/script.js
@@ -69,9 +69,25 @@ function generateTitleLinks() {
 
 generateTitleLinks();
 
+const calculateTagsParams = function(tags){
+  const params = {
+    max: 0,
+    min: 999999,
+  };
+
+  for(let tag in tags){
+    params.max = Math.max(tags[tag], params.max);
+    params.min = Math.min(tags[tag], params.min);
+
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+  }
+
+  return params;
+};
+
 function generateTags(){
-  /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
+  /* [NEW DONE] create a new variable allTags with an empty object */
+  let allTags = {};
   /* [DONE] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   console.log('articles', articles);
@@ -101,10 +117,12 @@ function generateTags(){
       /* [DONE] add generated code to html variable */
       let html = html + linkHTML;
       console.log(html);
-      /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
-        /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
+      /* [NEW DONE] check if this link is NOT already in allTags */
+      if(!allTags[tag]){
+        /* [NEW DONE] add tag to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
       }
     /* [DONE] END LOOP: for each tag */
     }
@@ -115,8 +133,21 @@ function generateTags(){
   /* [NEW] find list of tags in right column */
   const tagList = document.querySelector(optTagsListSelector);
 
-  /* [NEW] add html from allTags to tagList */
-  tagList.innerHTML = allTags.join(' ');
+  /* [NEW] create variable for all links HTML code */
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams);
+
+  let allTagsHTML = '';
+  /* [NEW] START LOOP: for each tag in allTags: */
+  for(let tag in allTags){
+  /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+  }
+  /* [NEW] END LOOP: for each tag in allTags: */
+
+  /*[NEW] add HTML from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
+  console.log(allTags);
 }
 generateTags();
 
